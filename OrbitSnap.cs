03abfea29eb517488ede481e14138c68b48cda9b
Mark Parker <godefroi@gmail.com>
2015-03-20 20:44:54 -0600

    Remove some non-functioning obsolete code
    The removed code describes my initial attempts at editing orbits, and
    shows how I had initially hoped to update the orbit as soon as the
    player moved away from a vessel. It didn't work.

--- OrbitSnap.cs
+++ OrbitSnap.cs
@@ -17,9 +17,6 @@ namespace OrbitSnap
 		 */
 		private void Awake()
 		{
-			//GameEvents.onVesselLoaded.Add(VesselLoaded);
-			//GameEvents.onVesselDestroy.Add(VesselUnloaded);
-
 			/*foreach( var cb in FlightGlobals.Bodies )
 			{
 				Debug.Log(string.Format("Celestial body {0} name={1} theName={2}, bodyName={3}", FlightGlobals.Bodies.IndexOf(cb), cb.name, cb.theName, cb.bodyName));
@@ -54,49 +51,12 @@ namespace OrbitSnap
 			};
 		}
 
-		private void VesselLoaded(Vessel v)
-		{
-			Debug.Log(string.Format("OrbitSnap: VesselLoaded vessel: {0} ({1})", v.name, v.id));
-
-			ShowOrbit(v.orbit);
-		}
-
-		private void VesselUnloaded(Vessel v)
-		{
-			Debug.Log(string.Format("OrbitSnap: VesselUnloaded vessel: {0} ({1}), loaded={2}", v.name, v.id, v.loaded));
-
-			if( v.loaded )
-			{
-				//UpdateOrbit(v, "destroyed");
-			}
-			//else if( m_pendmod.Contains(v.id) )
-			//{
-				//UpdateOrbit(v.id);
-			//}
-		}
-
 		/*
 		 * Called next.
 		 */
 		private void Start()
 		{
 			DontDestroyOnLoad(this);
-			//Debug.Log("OrbitSnap [" + this.GetInstanceID().ToString("X") + "][" + Time.time.ToString("0.0000") + "]: Start");
-
-			/*if( FlightGlobals.ActiveVessel != m_vessel )
-			{
-				if( m_vessel != null )
-					Debug.Log(string.Format("old current vessel was {0}", m_vessel.name));
-				else
-					Debug.Log("old current vessel was null");
-
-				if( FlightGlobals.ActiveVessel != null )
-					Debug.Log(string.Format("new current vessel is {0}", FlightGlobals.ActiveVessel.name));
-				else
-					Debug.Log("new current vessel is null");
-
-				m_vessel = FlightGlobals.ActiveVessel;
-			}*/
 		}
 
 		/*
@@ -175,13 +135,6 @@ namespace OrbitSnap
 			}
 		}
 
-		/*
-		 * Called when the game is leaving the scene (or exiting). Perform any clean up work here.
-		 */
-		/*private void OnDestroy()
-		{
-		}*/
-
 		private void ShowOrbit(Orbit orb)
 		{
 			Debug.Log(string.Format("Semi-major axis: {0}", orb.semiMajorAxis));
@@ -191,88 +144,6 @@ namespace OrbitSnap
 			Debug.Log(string.Format("Longitude of AN: {0}", orb.LAN));
 			Debug.Log(string.Format("Mean Anomaly at Epoch: {0}", orb.meanAnomalyAtEpoch));
 			Debug.Log(string.Format("Epoch: {0}", orb.epoch));
-		}
-
-		private void UpdateOrbit(Guid id)
-		{
-			var vessel = FlightGlobals.fetch.vessels.SingleOrDefault(v => v.id == id);
-
-			try
-			{
-				OrbitPhysicsManager.HoldVesselUnpack(60);
-			}
-			catch( NullReferenceException )
-			{
-				Debug.Log("OrbitSnap: OrbitPhysicsManager.HoldVesselUnpack threw NullReferenceException");
-			}
-
-			var allVessels = FlightGlobals.fetch == null ? (IEnumerable<Vessel>)new[] { vessel } : FlightGlobals.Vessels;
-			foreach( var vs in allVessels.Where(v => v.packed == false) )
-				vs.GoOnRails();
-
-			UpdateOrbit(vessel, "pending and not loaded");
-		}
-
-		private void UpdateOrbit(Vessel v, string when)
-		{
-			Debug.Log(string.Format("OrbitSnap: updating orbit for vessel {0} at {1}, packed={2}, loaded={3}", v.name, when, v.packed, v.loaded));
-
-			if( v == null )
-				return;
-
-			ShowOrbit(v.orbit);
-
-			try
-			{
-				OrbitPhysicsManager.HoldVesselUnpack(60);
-			}
-			catch( NullReferenceException )
-			{
-				Debug.Log("OrbitSnap: OrbitPhysicsManager.HoldVesselUnpack threw NullReferenceException");
-			}
-
-			var allVessels = FlightGlobals.fetch == null ? (IEnumerable<Vessel>)new[] { v } : FlightGlobals.Vessels;
-			foreach( var vs in allVessels.Where(vsl => vsl.packed == false) )
-				vs.GoOnRails();
-
-			var orbit = v.orbitDriver.orbit;
-
-			/*var no = new Orbit(0, 0, 1376600, orbit.LAN, orbit.argumentOfPeriapsis, orbit.meanAnomalyAtEpoch, orbit.epoch, orbit.referenceBody);
-
-			no.Init();
-			no.UpdateFromUT(Planetarium.GetUniversalTime());
-
-			Debug.Log("new orbit:");
-			ShowOrbit(no);*/
-
-			// inclination
-			orbit.inclination = 0;
-
-			// eccentricity
-			orbit.eccentricity = 0;
-
-			// semi-major axis
-			orbit.semiMajorAxis = 1376600;
-
-			// longitude of ascending node
-			//orb.LAN
-
-			// mean anomaly at epoch
-			//orb.meanAnomalyAtEpoch
-			
-			// epoch
-			//orb.epoch
-
-			orbit.Init();
-			orbit.UpdateFromUT(Planetarium.GetUniversalTime());
-
-			v.orbitDriver.pos = v.orbit.pos.xzy;
-			v.orbitDriver.vel = v.orbit.vel;
-
-			//v.orbit.UpdateFromOrbitAtUT(no, Planetarium.GetUniversalTime(), no.referenceBody);
-
-			//v.SetPosition(no.pos);
-			//v.SetWorldVelocity(no.vel);
 		}
 
 		private Orbit FindMatchingOrbit(Orbit currentOrbit)
